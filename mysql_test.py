ff06dd01af4dcc16e89a6eec9b6b8be6412849ac
jmaddock <skimaddster@gmail.com>
2013-08-05 08:13:47 -0700

    added samplers.py

--- mysql_test.py
+++ mysql_test.py
@@ -9,7 +9,7 @@ db = MySQLdb.connect(host=db_info.host, # your host, usually localhost
                       db=db_info.db) # name of the data base
 
 cur = db.cursor() 			#cursor object for mysql query
-dataset = []				#list of data entries
+twitter_punct = punctuation.replace('#','')	
 
 # calculate the number of tweets for each hour and write to a .csv
 def tweets_vs_time(cur):
@@ -27,7 +27,7 @@ def tweets_vs_time(cur):
 				f.write(result)
 
 	print 'done'
-
+ 
 #calculate most common words
 def word_frequency(cur):
 	f = open('word_frequency.csv', 'w')	#create new csv
@@ -68,29 +68,38 @@ def hashtag_frequency(cur):
 		print 'row: %d' % i
 
 	for x in count.most_common(1000):
-		result = '%s,%s\n' % (x[0],x[1])
+		result = '"%s","%s"\n' % (x[0],x[1])
 		f.write(result)	
 
 #calculate most common unique hashtags (not retweeted, filtered with RT)
-def unique_hashtag_frequency(cur):
-	f = open('unique_hashtag_frequency.csv', 'w')	#create new csv
-	f.write('hashtag,count\n')
+def hashtag_histogram(cur):
+	f = open('hashtag_histogram.csv', 'w')	#create new csv
+	f.write('hashtag,number of tweets,number of unique tweets,number of unique authors\n')
 	count = collections.Counter()
+	unique_count = collections.Counter()
+	authors = {}
 	#stopwords = "a,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your,yesterday,today".split(',')
 
-	cur.execute("select text from tweets where text like '%#%'")
+	cur.execute("select text,author from tweets where text like '%#%'")
 	print 'query done'
 	for (i,row) in enumerate(cur.fetchall()):
 		s = row[0].split()
-		if not 'RT' in s:
-			result = [x.lower() for x in s if x.find('#') > -1]
-				#if x in stopwords:
-				#	s.remove(x)
-			count.update(result)
+		result = [x.lower().strip(twitter_punct) for x in s if (x.find('#') > -1 and len(x) > 2)]
+		count.update(result)
+		if not ('RT @' or 'via @' or '\"@') in s:
+			unique_result = [z.lower().strip(twitter_punct) for z in s if (z.find('#') > -1 and len(z) > 2)]
+			print unique_result
+			print row
+			unique_count.update(unique_result)
+			for y in unique_result:
+				if not y in authors:
+					authors[y] = [row[1]]
+				elif row[1] not in authors[y]:
+					authors[y].append(row[1])
 		print 'row: %d' % i
 
 	for x in count.most_common(1000):
-		result = '%s,%s\n' % (x[0],x[1])
+		result = '"%s","%s","%s","%s",\n' % (x[0],x[1],unique_count[x[0]],len(authors[x[0]])) #TODO: get count of item from unique counter
 		f.write(result)	
 
 #most common hashtags, but by unique author and without retweets
@@ -118,12 +127,30 @@ def unique_author_hashtag_frequency(cur):
 		print 'row: %d' % i
 
 	for x in sorted(authors.items(), key=lambda x: len(x[1]), reverse=True):
-		result = '%s,%s\n' % (x[0],len(x[1]))
+		result = '"%s","%s"\n' % (x[0],len(x[1]))
 		f.write(result)	
+
+# get time series data for a particular hashtag
+def hashtag_frequency_over_time(cur):
+	tag = "#tgdn"
+
+	title = "%s_over_time.csv" % tag
+	f = open(title, 'w')	#create new csv
+	f.write('time,%s\n' % tag) 
+	print tag
+	for i in range(15,23):		#15-23 (day)
+		for j in range(0,24):	#0-24 (hour)
+			dateStart = '2013-04-%02d %02d'  % (i,j)
+			print "time: %s" % dateStart
+			print "tag: " + tag
+			cur.execute("select count(*) from tweets where (text like '%" + tag + "%' and time like '" + dateStart + "%')")
+			for row in cur.fetchall():
+				result = '"%s",%d\n' % (dateStart,row[0])
+				f.write(result)
 
 #calculate hashtag use over time
 #TODO: output formatting could use work
-def hashtag_frequency_over_time(cur):
+def top_hashtag_frequency_over_time(cur, tag):
 	f = open('hashtag_frequency_over_time.csv', 'w')	#create new csv
 	f.write('hashtag,time,count\n')
 	count = collections.Counter()
@@ -147,7 +174,7 @@ def hashtag_frequency_over_time(cur):
 				print "tag: " + str(x[0])
 				cur.execute("select count(*) from tweets where (text like '%" + str(x[0]) + "%' and time like '" + dateStart + "%')")
 				for row in cur.fetchall():
-					result = '%s,%s,%d\n' % (x[0],dateStart,row[0])
+					result = '"%s","%s",%d\n' % (x[0],dateStart,row[0])
 					f.write(result)
 
 #ignores mentions in retweets and multiple mentions by the same author
@@ -175,7 +202,7 @@ def unique_mention_frequency(cur):
 		print 'row: %d' % i
 
 	for x in sorted(authors.items(), key=lambda x: len(x[1]), reverse=True)[:1000]:
-		result = '%s,%s\n' % (x[0],len(x[1]))
+		result = '"%s","%s"\n' % (x[0],len(x[1]))
 		f.write(result)	
 
 def place(cur):
@@ -256,32 +283,57 @@ def hashtag_network(cur):
 	print 'query done'
 	for (i,row) in enumerate(cur.fetchall()):
 		s = row[0].split()
-		result = [x.lower() for x in s if x.find('#') > -1]
+		result = [x.lower().strip(twitter_punct) for x in s if (x.find('#') > -1 and len(x) > 2)]
 			#if x in stopwords:
 			#	s.remove(x)
 		count.update(result)
 		#print 'row: %d' % i
 
-	for x in count.most_common(100):
-		result = '%s,%s\n' % (x[0],x[1])
+	for x in count.most_common(1000):
+		result = '"%s","%s"\n' % (x[0],x[1])
 		f.write(result)
 
 	f.write('EDGES\n')
-	tags = {z[0]:0 for z in count.most_common(100)}
-	for x in count.most_common(100):
+	tags = {z[0]:0 for z in count.most_common(1000)}
+	for x in count.most_common(1000):
 		edge_counter = collections.Counter(tags)	#add count(100) with function to get only counts of existing hashtags
 		cur.execute("select text from tweets where text like '%" + x[0] + "%'")
 		for (i,row) in enumerate(cur.fetchall()):
 			s = row[0].split()
-			result = [y.lower() for y in s if (y.find('#') > -1)]
+			result = [y.lower().strip(twitter_punct) for y in s if (y.find('#') > -1 and len(y) > 2)]
 			print result
 			edge_counter.update(result)
 			#print 'row: %d' % i
 
-		for y in edge_counter.most_common(100): 
-			result = '%s,%s,%s\n' % (x[0],y[0],y[1])
+		for y in edge_counter.most_common(1000): 
+			result = '"%s","%s","%s"\n' % (x[0],y[0],y[1])
 			f.write(result)	 
+
+def single_node_network(cur):
+	node = "#tcot"
+	title = "%s_node_network.csv" % node
+	f = open(title, 'w')	#create new csv
+	count = collections.Counter()
+	#stopwords = "a,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your,yesterday,today".split(',')
+
+	cur.execute("select count(*) from tweets where text like '%" + node + "%'")
+	f.write("NODE\n")
+	f.write('"%s","%s"\n' % (node,cur.fetchall()[0][0]))
+	
+	f.write('EDGES\n')
+	edge_counter = collections.Counter()	#add count(100) with function to get only counts of existing hashtags
+	cur.execute("select text from tweets where text like '%" + node + "%'")
+	for (i,row) in enumerate(cur.fetchall()):
+		s = row[0].split()
+		print s
+		result = [y.lower().strip(twitter_punct) for y in s if (y.find('#') > -1 and len(y) > 2)]
+		edge_counter.update(result)
+		#print 'row: %d' % i
+
+	for y in edge_counter.most_common(100): 
+		result = '"%s","%s","%s"\n' % (node,y[0],y[1])
+		f.write(result)	 
 
 
 if __name__ == "__main__":
-    hashtag_frequency_over_time(cur)
+    single_node_network(cur)
