501b2240796bc900ce059d451f372b476dd67773
jmaddock <skimaddster@gmail.com>
2014-01-16 22:47:45 -0800

    added url specific collection method

--- mongo/mongo_update.py
+++ mongo/mongo_update.py
@@ -14,12 +14,27 @@ sql_db = MySQLdb.connect(host="localhost",
                          db="Jfk")
 sql_cursor = sql_db.cursor()
 
-written_ids = open('written_ids_proposal.txt','w')
-#sql db query
-sql_cursor.execute("select id,code from tweets_proposal")
-for x in sql_cursor.fetchall():
-        query = str(x[0])
-        value = str(x[1])
-        print query,value
-        written_ids.write('"%s","%s"\n' % (query,value))
-        tweets.update({'user.id':query},{'$push':{'codes':{'rumor':'proposal','code':value}}})
+def code_import():
+        written_ids = open('written_ids_proposal.txt','w')
+        #sql db query
+        sql_cursor.execute("select id,code from tweets_proposal")
+        for x in sql_cursor.fetchall():
+                query = str(x[0])
+                value = str(x[1])
+                print query,value
+                written_ids.write('"%s","%s"\n' % (query,value))
+                tweets.update({'user.id':query},
+                              {'$push':{'codes':{'rumor':'proposal',
+                                                 'code':value}}})
+
+def create_rumor_collection():
+
+        # enter new collection info
+        # assumes collection has already been created
+        collection2 = mongo_db.jfk
+        jfk = mongo_db.jfk
+
+        sql_cursor.execute("select id from tweets_proposal")
+        for x in sql_cursor:
+                document = tweets.find_one({'user.id':str(x)})
+                jfk.insert(document)
