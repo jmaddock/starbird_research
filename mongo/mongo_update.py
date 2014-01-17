1aaed5544ca5b411c4e173ba9ef67877537611d3
jmaddock <skimaddster@gmail.com>
2014-01-17 00:45:40 -0800

    finished collection creation for specific rumors

--- mongo/mongo_update.py
+++ mongo/mongo_update.py
@@ -10,8 +10,8 @@ tweets = mongo_db.tweets
 #connect to sql db
 sql_db = MySQLdb.connect(host="localhost",
                          user="root",
-                         passwd="Vwg0lf9!",
-                         db="Jfk")
+                         passwd="",
+                         db="jfk_and_proposal")
 sql_cursor = sql_db.cursor()
 
 def code_import():
@@ -31,10 +31,20 @@ def create_rumor_collection():
 
         # enter new collection info
         # assumes collection has already been created
-        collection2 = mongo_db.jfk
-        jfk = mongo_db.jfk
+        collection2 = mongo_db.proposal
+        proposal = mongo_db.proposal
 
+        #logging
+        written_ids = open('log_proposal.txt','w')
+
+        #query sql, pull document from tweets collection, and add document to
+        #new collection
         sql_cursor.execute("select id from tweets_proposal")
-        for x in sql_cursor:
-                document = tweets.find_one({'user.id':str(x)})
-                jfk.insert(document)
+        for i,x in enumerate(sql_cursor):
+                print i
+                written_ids.write('"%s"\n' % (x[0]))
+                document = tweets.find_one({'user.id':str(x[0])})
+                proposal.insert(document)
+
+if __name__ == "__main__":
+    create_rumor_collection()
