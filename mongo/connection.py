6b4ab5d588d4c63ccd17d66926dd8dc738946ba2
Jim Maddock <maddock@boston.ischool.uw.edu>
2014-02-10 00:42:04 -0800

    added db config file

--- mongo/connection.py
+++ mongo/connection.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import MySQLdb
+import db_info as DB
 
 class dbConnection(object):
 
@@ -9,15 +10,15 @@ class dbConnection(object):
 
     def create_mongo_connections(self,options=[]):
         if 'boston' in options:
-            self.m_connections['boston'] = MongoClient(host='10.60.0.51').boston.tweets
+            self.m_connections['boston'] = MongoClient(host=DB.mongo['host']).boston.tweets
 
         if 'new_boston' in options:
-            self.m_connections['new_boston'] = MongoClient(host='10.60.0.51').new_boston.tweets
+            self.m_connections['new_boston'] = MongoClient(host=DB.mongo['host']).new_boston.tweets
 
     def create_sql_connections(options=[]):
         if 'boston' in options:
-            sql_db = MySQLdb.connect(host="localhost",
-                                     user="jim",
-                                     passwd="sidewinder11",
-                                     db="boston")
+            sql_db = MySQLdb.connect(host=DB.sql['host'],
+                                     user=DB.sql['user'],
+                                     passwd=DB.sql['password'],
+                                     db=DB.sql['db'])
             self.sql_connections = sql_cursor = sql_db.cursor()
