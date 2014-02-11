a814b877da76b39f09240c40dbb4106cc98fbc55
Jim Maddock <maddock@boston.ischool.uw.edu>
2014-02-10 23:08:58 -0800

    working connection module

--- mongo/connection.py
+++ mongo/connection.py
@@ -8,17 +8,17 @@ class dbConnection(object):
        self.m_connections = {}
        self.sql_connections = {}
 
-    def create_mongo_connections(self,options=[]):
-        if 'boston' in options:
+    def create_mongo_connections(self,mongo_options=[]):
+        if 'boston' in mongo_options:
             self.m_connections['boston'] = MongoClient(host=DB.mongo['host']).boston.tweets
 
-        if 'new_boston' in options:
+        if 'new_boston' in mongo_options:
             self.m_connections['new_boston'] = MongoClient(host=DB.mongo['host']).new_boston.tweets
 
-    def create_sql_connections(options=[]):
-        if 'boston' in options:
+    def create_sql_connections(self,sql_options=[]):
+        if 'boston' in sql_options:
             sql_db = MySQLdb.connect(host=DB.sql['host'],
                                      user=DB.sql['user'],
                                      passwd=DB.sql['password'],
                                      db=DB.sql['db'])
-            self.sql_connections = sql_cursor = sql_db.cursor()
+            self.sql_connections['boston'] = sql_db.cursor()
