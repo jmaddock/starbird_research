b8eaa0f58c9a4bf3367fa6ef67a9ac352338ae53
jmaddock <skimaddster@gmail.com>
2014-02-17 11:35:33 -0800

    updated long-url-repair

--- mongo/url_dev.py
+++ mongo/url_dev.py
@@ -2,10 +2,10 @@ from pymongo import MongoClient
 from datetime import datetime
 import collections,re,random
 
-client = MongoClient()
-db = client.boston
-collection = db.jfk
-tweets = db.jfk
+client = MongoClient('z')
+db = client.new_boston
+collection = db.tweets
+tweets = db.tweets
 
 main_collection = db.tweets
 main_tweets = db.tweets
@@ -165,6 +165,104 @@ def url_repair():
     print count
     f.write('%s' % count)
 
+def long_url_repair():
+    # set up counter, logging.
+    f = open('data/long_url_matches_test.csv','w')
+    f.write('repaired tweet id,reference tweet id\n')
+    f2 = open('data/long_url_error_log.csv','w')
+    count = collections.Counter()
+    bad_links = []
+
+    # get all tweets with any number of known urls
+    # if the long-url = expanded_url, add to "bad_urls"
+    # expansion errors recorded in data/log_url_error_log
+    # data = tweets.find({'counts.urls':{'$gte':1}}).limit(10000)
+    data = tweets.find({'id':'324976766118662144'})
+    for x in data:
+        print data
+        for y in x['entities']['urls']:
+            try:
+                if y['long-url'] == y['expanded_url']:
+                    bad_links.append(x)
+            except:
+                f2.write('%s\n' % x['id'])
+
+    count.update({'bad links':len(bad_links)})
+    sample = [bad_links[0]]
+    print 'done'
+
+    # iterate through bad links, mentions and urls
+    # find a document where the long-url does not contain the expanded_url text
+    # if there are multiple, pick the first by time
+    for i,y in enumerate(sample):
+        print y
+        text = y['text']
+        start_text = ''
+        while start_text is not text:
+            start_text = text
+            text = text.rstrip('...').strip()
+            text = re.sub('RT .*?:','',text).strip()
+            text = re.sub('http.*?\s|http.*?$','',text).strip()
+        print text
+        tweet_text = re.compile(re.escape(text))
+        for z in y['entities']['urls']:
+            if z['long-url'] == z['expanded_url']:
+                link_text = z['expanded_url']
+                reg_link_text = re.compile(z['expanded_url'])
+                print link_text,reg_link_text
+        new_data = tweets.find({'text':tweet_text,
+                                'entities.urls.expanded_url':reg_link_text,
+                                'entities.urls.long-url':{'$not':reg_link_text}}).sort('created_ts',1).limit(1)
+
+        try:
+            new_data = new_data.next()
+            print "sucess"
+        except:
+            new_data = None
+            print "fail"
+        print 'done'
+        # if there are no matches, add to unmatched
+        # else find matching document in db and update url entry
+        if new_data == None:
+            count.update(['unmatched'])
+        else:
+            count.update(['matched'])
+            f.write('%s,%s\n' % (y['user']['id'],new_data['user']['id']))
+            for z in new_data['entities']['urls']:
+                if reg_link_text.search(z['expanded_url']) != None:
+                    z['reconstructed'] = True
+                    #print z
+                    #main_tweets.update({'user.id':y['user']['id']},
+                    #                   {'$push':{'entities.urls':z}})
+                    main_tweets.update({'user.id':y['user']['id'],
+                                        'entities.urls.expanded_url':reg_link_text},
+                                       {'$set':{'entities.urls.$':z}})
+
+    print count
+    f.write('%s' % count)
+
+def test():
+    f = open('data/long_url_matches_test.csv','w')
+    f.write('repaired tweet id,reference tweet id\n')
+    f2 = open('data/long_url_error_log.csv','w')
+    count = collections.Counter()
+    bad_links = []
+
+    # get all tweets with any number of known urls
+    # if the long-url = expanded_url, add to "bad_urls"
+    # expansion errors recorded in data/log_url_error_log
+    data = tweets.find({'counts.urls':{'$gte':1}}).limit(10000)
+    for x in data:
+        for y in x['entities']['urls']:
+            try:
+                if y['long-url'] == y['expanded_url']:
+                    bad_links.append(x)
+            except:
+                f2.write('%s\n' % x['_id'])
+
+    count.update({'bad links':len(bad_links)})
+    print bad_links[0]
+
 def count_RT():
     num_RT = collections.Counter()
     reg1 = re.compile('rt @',re.IGNORECASE)
@@ -179,4 +277,4 @@ def count_RT():
     print num_RT
 
 if __name__ == "__main__":
-    url_repair()
+    long_url_repair()
