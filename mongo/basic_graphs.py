c4b54f3707707a5228825c3e72f08ac9d7c8ca54
jmaddock <skimaddster@gmail.com>
2014-02-18 13:29:03 -0800

    added fine grained coding scheme

--- mongo/basic_graphs.py
+++ mongo/basic_graphs.py
@@ -2,14 +2,17 @@ from connection import dbConnection
 from collections import Counter
 from datetime import datetime
 
-def rumor_over_time(db_name,rumor):
+def rumor_over_time(db_name,rumor,gran):
     db = dbConnection()
     db.create_mongo_connections(mongo_options=[db_name])
 
     title = "data/%s_over_time.csv" % rumor.replace('/','_')
     f = open(title, 'w')
+    if gran:
+        f.write('time,misinfo,correction,speculation,hedge,question,unrelated/neutral/other\n')
+    else:
+        f.write('time,misinfo,correction,unrelated/neutral/other\n')
 
-    f.write('time,misinfo,correction,unrelated/neutral/other\n')
     for i in range(15,23):      #15-23 (day)
         for j in range(0,24):   #0-24 (hour)
             for k in range(0,60,10):
@@ -29,16 +32,36 @@ def rumor_over_time(db_name,rumor):
                 })
                 for x in raw_data:
                     count.update([x['codes'][0]['code']])
-                misinfo = count['misinfo'] + count['speculation'] + count['hedge']
-                correction = count['correction'] + count['question']
-                other = count['unrelated'] + count['other/unclear/neutral'] + count['unclear'] + count[''] + count['discussion - justifying'] + count['discussion - question'] + count['other'] + count['discussion']
-                result = '"%s",%d,%d,%d\n' % (dateStart,misinfo,correction,other)
-                f.write(result)
+
+                    if gran:
+                        misinfo = count['misinfo']
+                        speculation = count['speculation']
+                        hedge = count['hedge']
+                        correction = count['correction']
+                        question = count['question']
+                        other = count['unrelated'] + count['other/unclear/neutral'] + count['unclear'] + count[''] + count['discussion - justifying'] + count['discussion - question'] + count['other'] + count['discussion']
+                        result = '"%s",%d,%d,%d,%d,%d,%d\n' % (dateStart,
+                                                               misinfo,
+                                                               correction,
+                                                               speculation,
+                                                               hedge,
+                                                               question,
+                                                               other)
+                    else:
+                        misinfo = count['misinfo'] + count['speculation'] + count['hedge']
+                        correction = count['correction'] + count['question']
+                        other = count['unrelated'] + count['other/unclear/neutral'] + count['unclear'] + count[''] + count['discussion - justifying'] + count['discussion - question'] + count['other'] + count['discussion']
+                        result = '"%s",%d,%d,%d\n' % (dateStart,
+                                                      misinfo,
+                                                      correction,
+                                                      other)
+
+                    f.write(result)
 
 def main():
     rumors = ['girl running','sunil','seals/craft','cell phone','proposal','jfk']
     for x in rumors:
-        rumor_over_time(db_name='new_boston',rumor=x)
+        rumor_over_time(db_name='new_boston',rumor=x,gran=True)
 
 if __name__ == "__main__":
     main()
